60bb8f757be1ba34a26b24e49eac26ff20d37101
Sibian De Gussem <sibian.degussem@ugent.be>
2020-05-06 16:24:52 +0200

    H15 start

--- H15/Theorie/H15Oef1Start/src/ui/RapportApplicatie.java
+++ H15/Theorie/H15Oef1Start/src/ui/RapportApplicatie.java
@@ -1,14 +1,20 @@
 package ui;
 
 import domein.OrderRecord;
+
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.InvalidPathException;
+import java.nio.file.Paths;
 import java.util.Formatter;
+import java.util.FormatterClosedException;
 import java.util.Scanner;
 
 public class RapportApplicatie
 {
 
-    public static final String INNAAM = "order.txt";
-    public static final String UITNAAM = "korting.txt";
+    public static final String INNAAM = "../order.txt";
+    public static final String UITNAAM = "../korting.txt";
     public static final int MINAANTAL = 11;
     private Scanner input;
     private Formatter output;
@@ -23,16 +29,35 @@ public class RapportApplicatie
 
     private void openFiles()
     {
-
+        try {
+            input = new Scanner(Files.newInputStream(Paths.get(INNAAM)));
+            output = new Formatter((Files.newOutputStream(Paths.get(UITNAAM))));
+        } catch (InvalidPathException ie){
+            System.out.println("Error fiding path");
+        } catch (IOException filesnotfound){
+            System.out.println("Error reading file");
+        }
     }
 
     private void kopieerKlantenMetKorting()
     {
-                 
+        try {
+            while (input.hasNext()){
+                OrderRecord orderRecord = new OrderRecord(input.next(), input.next(), input.nextInt(), input.nextDouble());
+                if (orderRecord.getAantal() > MINAANTAL){
+                    output.format("%s %s %d %.2f",
+                            orderRecord.getNaam(), orderRecord.getProduct(), orderRecord.getAantal(), (orderRecord.getAantal()*orderRecord.getPrijs()*.95));
+                }
+            }
+        } catch (FormatterClosedException formatterClosedException){
+            System.out.println("Error writing file.");
+        }
     }
 
     private void closeFiles()
     {
-         
+         if (input != null){
+             input.close();
+         }
     }
 }
